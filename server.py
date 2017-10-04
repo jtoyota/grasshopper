37fcbbfef53287d3939dacd7603c562a3faf3c3c
mbichoffe <bichoffe.marina@gmail.com>
2017-10-03 17:02:28 -0700

    created splash page

--- server.py
+++ server.py
@@ -4,12 +4,32 @@ from jinja2 import StrictUndefined
 
 from flask import Flask, render_template, request, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
-
+from model import * 
 
 app = Flask(__name__)
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "BURNAFTERREADING"
+
+@app.route('/')
+def index():
+    """Splash page."""
+
+    return render_template("/index.html")
+
+@app.route('/about')
+def about_page():
+
+    render_template("/about")
+
+@app.route('/mentor_registration/')
+def mentor_registration():
+    pass
+
+@app.route('/mentee_registration/')
+def mentee_registration():
+    pass
+
 
 # Normally, if you use an undefined variable in Jinja2, it fails silently.
 # This is horrible. Fix this so that, instead, it raises an error.
@@ -24,4 +44,4 @@ if __name__ == "__main__":
     # Use the DebugToolbar
     DebugToolbarExtension(app)
 
-    app.run(host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
