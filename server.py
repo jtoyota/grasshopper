f3c9088ef99505fdcc45a4b4f76be2882f840d99
mbichoffe <bichoffe.marina@gmail.com>
2017-10-04 19:20:40 -0700

    added areas of interest page and created server route to store values

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
-#marinabichoffe hackbright final project
-from jinja2 import StrictUndefined
+# marinabichoffe hackbright final project
 
-from flask import Flask, render_template, request, flash, redirect, session, jsonify
+from jinja2 import StrictUndefined
+from flask import Flask, render_template, request, flash, redirect, session, \
+    jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from model import * 
 
@@ -11,24 +12,52 @@ app = Flask(__name__)
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "BURNAFTERREADING"
 
+
 @app.route('/')
 def index():
     """Splash page."""
 
     return render_template("/index.html")
 
-@app.route('/about')
-def about_page():
 
-    render_template("/about")
+@app.route('/about/')
+def about():
+    """Info about the program."""
+
+    return render_template("/about")
 
 @app.route('/mentor_registration/')
 def mentor_registration():
-    pass
+    session['user_type'] = 'mentor'
+    return render_template('mentor_register.html')
+
+@app.route('/areas_of_interest/', methods=['POST', 'GET'])
+def get_areas_of_interest():
+    if request.method == 'POST':
+
+        inputs = request.form.get('my_style')
+        return "this"
+
+    return render_template('/areas_of_interest.html')
 
 @app.route('/mentee_registration/')
 def mentee_registration():
     pass
+
+    # melon = request.form.get('melon_type')
+    # qty = int(request.form.get('qty'))
+
+    # if qty > 10:
+    #     result_code = 'ERROR'
+    #     result_text = "You can't buy more than 10 melons"
+    # elif qty > 0:
+    #     result_code = 'OK'
+    #     result_text = "You have bought %s %s melons" % (qty, melon)
+    # else:
+    #     result_code = 'ERROR'
+    #     result_text = "You want to buy fewer than 1 melons? Huh?"
+
+
 
 
 # Normally, if you use an undefined variable in Jinja2, it fails silently.
---
+++ static/js/mentor_register.js
@@ -0,0 +1,85 @@
+
+//Function to handle submission of areas of interest questionnaire
+$("#areas_of_interest").on('submit', function(evt) {
+    // debugger;
+    debugger;
+    evt.preventDefault();
+
+    var formInputs = $("#areas_of_interest").serialize();
+    
+    console.log(formInputs);
+    $.post("/areas-of-interest", formInputs, function (results) {
+        console.log("success!")
+        // if (results.code == "OK") {
+        //     $('#order-status').html("<p>" + results.msg + "</p>");
+        // }
+        // else {
+        //     $('#order-status').addClass("order-error");
+        //     $('#order-status').html("<p><b>" + results.msg + "</b></p>");
+        });
+});
+// function orderMelons(evt) {
+//     evt.preventDefault();
+
+//     var formInputs = $("#order-form").serialize();
+//     $.post("/order-melons.json", formInputs, function (results) {
+//         if (results.code == "OK") {
+//             $('#order-status').html("<p>" + results.msg + "</p>");
+//         }
+//         else {
+//             $('#order-status').addClass("order-error");
+//             $('#order-status').html("<p><b>" + results.msg + "</b></p>");
+//         }
+//     });
+// }
+
+// $("#order-form").on('submit', orderMelons);
+// // PART 1: SHOW A FORTUNE
+
+// // Written now to use an inline function for the event handler,
+// // and to use the $.load() method
+
+// $('#get-fortune-button').on('click', function (evt) {
+//     $("#fortune-text").load('/fortune');
+// });
+
+
+// // PART 2: SHOW WEATHER
+
+// // Written now to use an inline function for handling the
+// // AJAX success handler.
+
+// function showWeather(evt) {
+//     evt.preventDefault();
+
+//     var url = "/weather.json?zipcode=" + $("#zipcode-field").val();
+
+//     $.get(url, function (results) {
+//         $("#weather-info").html(results.forecast);
+//     });
+// }
+
+// $("#weather-form").on('submit', showWeather);
+
+
+// // PART 3: ORDER MELONS
+
+// // Written now to use inline function for the handling the AJAX
+// // success handler, and to use $form.serialize
+
+// function orderMelons(evt) {
+//     evt.preventDefault();
+
+//     var formInputs = $("#order-form").serialize();
+//     $.post("/order-melons.json", formInputs, function (results) {
+//         if (results.code == "OK") {
+//             $('#order-status').html("<p>" + results.msg + "</p>");
+//         }
+//         else {
+//             $('#order-status').addClass("order-error");
+//             $('#order-status').html("<p><b>" + results.msg + "</b></p>");
+//         }
+//     });
+// }
+
+// $("#order-form").on('submit', orderMelons);
